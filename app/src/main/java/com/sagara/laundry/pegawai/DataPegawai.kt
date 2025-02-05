5211228c58c5f6b3eb7ed1cdba7b6eb9e64f1c6d
sagaradhylan1 <dilanparamaraja1@gmail.com>
2025-02-05 13:19:26 +0700

    penambahan untuk menyambungkan di hp agar dapat masuk ke pelanggan dan pegawai

--- app/src/main/java/com/sagara/laundry/pegawai/DataPegawai.kt
+++ app/src/main/java/com/sagara/laundry/pegawai/DataPegawai.kt
@@ -1,16 +1,17 @@
-package com.sagara.laundry
+package com.sagara.laundry.pegawai
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sagara.laundry.R
 
-class Card_Pelanggan : AppCompatActivity() {
+class DataPegawai : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_card_pelanggan)
+        setContentView(R.layout.activity_data_pegawai)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/sagara/laundry/pegawai/TambahPegawai.kt
+++ app/src/main/java/com/sagara/laundry/pegawai/TambahPegawai.kt
@@ -1,30 +1,17 @@
-package com.sagara.laundry.pelanggan
+package com.sagara.laundry.pegawai
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.sagara.laundry.R
 
-class DataPelanggan : AppCompatActivity() {
-
-    lateinit var tambah : FloatingActionButton
-
+class TambahPegawai : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_data_pelanggan)
-
-        tambah = findViewById(R.id.fabData_Pengguna_Tambah)
-
-        tambah.setOnClickListener{
-            val intent = Intent(this@DataPelanggan, TambahPelanggan:: class.java)
-            startActivity(intent)
-
-        }
+        setContentView(R.layout.activity_tambah_pegawai)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
